d2892ecf2cbe13425c82e07db29be77e8f1f6c85
Saumya Jain <saumya.jain2001@gmail.com>
2024-09-09 15:15:06 +0200

    changed port

--- modified-api.js
+++ modified-api.js
@@ -3,7 +3,7 @@ const { SessionsClient } = require('@google-cloud/dialogflow-cx');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // const projectId = 'ccap-gptfy-qa';
 // const location = 'global';
